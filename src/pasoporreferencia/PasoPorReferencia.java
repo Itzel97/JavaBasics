396d54e0945d4ebaa501a661c4cd2a07cbcf358a
Itzel Alonso <53825507+Itzel97@users.noreply.github.com>
2022-06-06 22:59:14 -0500

    return y null

--- src/pasoporreferencia/PasoPorReferencia.java
+++ src/pasoporreferencia/PasoPorReferencia.java
@@ -6,18 +6,26 @@ public class PasoPorReferencia {
 
     public static void main(String[] args) {
 
-        Persona persona1 = new Persona();
-        persona1.nombre = "itzel";
-        System.out.println("persona1 nombre = " + persona1.nombre);
-        cambiarValor(persona1);
-        System.out.println("persona1 nombre = " + persona1.nombre);
+        //Persona persona1 = new Persona();
+        Persona persona1=null;
+//        persona1.nombre = "itzel";
+//        System.out.println("persona1 nombre = " + persona1.nombre);
+        persona1 = cambiarValor(persona1);
+//        System.out.println("persona1 nombre = " + persona1.nombre);
 
     }
 
     // en este caso persona1 y el argumento persona, apuntan hacia el mismo objeto
     //en memoria
-    public static void cambiarValor(Persona persona) {
+    public static Persona cambiarValor(Persona persona) {
+        if (persona == null) {
+            System.out.println("Valor de persona igual a null");
+            return null;
+        }
+
         persona.nombre = "karla";
+        return persona;
+
     }
 // cambiaron el valor en la memoria del objeto por lo que no es una variable nativa
 // como tal es una referencia y puede leerlo con el cambio.
